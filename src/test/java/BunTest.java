4a39d51e91a4d21a7b40af2e47beecbd3b99c4d4
Ermak0vYurii <grantovod1@gmail.com>
2025-02-07 13:00:22 +0500

    Устранение замечаний

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,20 +1,45 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
-import static Constants.Constants.*;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class BunTest {
 
-    private final Bun bun = new Bun(NAME_BUN, PRICE_BUN);
+    private final String nameBun;
+    private final float priceBun;
+
+    public BunTest(String nameBun, Float priceBun) {
+        this.nameBun = nameBun;
+        this.priceBun = priceBun;
+    }
+
+    @Parameterized.Parameters(name = "nameBun: {0}, priceBun: {1}")
+    public static Object[][] testData() {
+        return new Object[][]{
+                {"black bun", 100F},
+                {"black bun", 0F},
+                {"black bun", -100F},
+                {"black bun", 158.99F},
+                {"Черная булка", 100F},
+                {"666 bun", 100F},
+                {"@#&[]", 100F},
+                {"", 100F},
+                {null, 100F},
+        };
+    }
 
     @Test
     public void getNameTest() {
-        assertEquals("Неверное название булки", NAME_BUN, bun.getName());
+        Bun bun = new Bun(nameBun, priceBun);
+        assertEquals("Неверное название булки", nameBun, bun.getName());
     }
 
     @Test
     public void getPriceTest() {
-        assertEquals("Неверная стоимость булки", PRICE_BUN, bun.getPrice(), 0);
+        Bun bun = new Bun(nameBun, priceBun);
+        assertEquals("Неверная стоимость булки", priceBun, bun.getPrice(), 0);
     }
 }
